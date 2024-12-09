a1d3446f2166364c8008a9fa8eed3250a1f45e39
IreneGarciaF <irenegfdez998@gmail.com>
2024-12-09 16:22:27 +0100

    cambio en el nombre de una imagen

--- src/data/cursos.js
+++ src/data/cursos.js
@@ -1,8 +1,7 @@
 const cursos = [
-    { id: 1, titulo: "Curso de React", descripcion: "Domina el desarrollo web moderno.", precio: "49.99 €", imagen: "src/assets/1*odW0CyTVxMVt5s3yhjjOhw.png"},
+    { id: 1, titulo: "Curso de React", descripcion: "Domina el desarrollo web moderno.", precio: "49.99 €", imagen: "src/assets/1_dW0CyTVxMVt5s3yhjjOhw.png"},
     { id: 2, titulo: "Curso de JavaScript", descripcion: "Aprende el lenguaje de la web.", precio: "39.99 €", imagen: "src/assets/gabriel-heinzer-g5jpH62pwes-unsplash-scaled.jpg"},
     { id: 3, titulo: "Curso de Diseño UX/UI", descripcion: "Diseña experiencias que atrapen a tus usuarios.", precio: "29.99 €", imagen: "src/assets/c00bce58c817ec3a16945711111641d37320ae67-2240x1260.webp"},
   ];
   
   export default cursos;
-  
